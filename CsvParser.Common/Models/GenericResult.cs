d7307608e7ce702db308701db46dff4890157e06
Αναστάσιος Παλασόπουλος <a_palasopoulos@yahoo.com>
2024-05-25 20:46:19 +0300

    To do an application transaction request

---
+++ CsvParser.Common/Models/GenericResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvParser.Common.Models
+{
+    public class GenericResult
+    {
+        public bool IsValid { get; set; }
+        public List<string> Errors { get; set; } = new List<string>();
+
+        public GenericResult() { }
+
+        public GenericResult(bool isValid, List<string> errors)
+        {
+            IsValid = isValid;
+            Errors = errors ?? new List<string>();
+        }
+    }
+}
