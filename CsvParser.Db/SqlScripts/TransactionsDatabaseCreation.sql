53d541ce4bfe67d53453a946e8827d714eb513d4
Αναστάσιος Παλασόπουλος <a_palasopoulos@yahoo.com>
2024-05-25 09:32:51 +0300

    Database Script

---
+++ CsvParser.Db/SqlScripts/TransactionsDatabaseCreation.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE Transactions;
+
+
+CREATE TABLE ApplicationTransaction (
+    Id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(), 
+    ApplicationName NVARCHAR(200) NOT NULL, 
+    Email NVARCHAR(200) NOT NULL, 
+    Filename NVARCHAR(300) NULL ,
+    Url NVARCHAR(2048) NULL ,
+    Inception DATETIME NOT NULL ,
+    Amount MONEY NOT NULL, 
+    Allocation DECIMAL(5, 2) NULL 
+);
