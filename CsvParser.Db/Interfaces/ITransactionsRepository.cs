145a14c2921470fc09cb393b433730ef95904e6c
Αναστάσιος Παλασόπουλος <a_palasopoulos@yahoo.com>
2024-05-25 14:58:35 +0300

    WIP

---
+++ CsvParser.Db/Interfaces/ITransactionsRepository.cs
@@ -0,0 +1,19 @@
+﻿using CsvParser.Db.DbEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvParser.Db.Interfaces
+{
+    public interface ITransactionsRepository
+    {
+        public void UpsertTransaction(ApplicationTransaction transaction);
+        public IEnumerable<ApplicationTransaction> GetTransactions(int page, int pageSize);
+
+        public ApplicationTransaction GetTransaction(Guid id);
+
+        public void DeleteTransaction(Guid id);
+    }
+}
---
+++ CsvParser.Db/Repository/TransactionsRepository.cs
@@ -0,0 +1,79 @@
+﻿using CsvParser.Db.Context;
+using CsvParser.Db.DbEntities;
+using CsvParser.Db.Interfaces;
+using Dapper;
+using System.Data.SqlClient;
+
+
+namespace CsvParser.Db.Repository
+{
+    public class TransactionsRepository:ITransactionsRepository
+    {
+        private readonly DapperContext _context;
+
+        public TransactionsRepository(DapperContext context)=>_context = context;
+
+        private static string UpsertTransactionsQuery = @" MERGE ApplicationTransactions AS target 
+                                                          USING (SELECT @Id AS Id) AS source
+                                                          ON (target.Id = source.Id) WHEN MATCHED THEN 
+                                                          UPDATE SET ApplicationName = @ApplicationName,
+                                                          Email = @Email, 
+                                                          Filename = @Filename,
+                                                          Url = @Url,
+                                                          Inception =@Inception, 
+                                                          Amount = @Amount,
+                                                          Allocation = @Allocation
+                                                          WHEN NOT MATCHED THEN
+                                                          INSERT (Id, ApplicationName, Email, Filename, Url, Inception, Amount, Allocation)
+                                                          VALUES (@Id, @ApplicationName, @Email, @Filename, @Url, @Inception, @Amount, @Allocation); 
+                                                          ";
+
+
+        private static string GetTransactionsQuery = @"
+                                                     SELECT * FROM ApplicationTransactions
+                                                     ORDER BY Inception
+                                                     OFFSET @Offset ROWS FETCH NEXT @PageSize ROWS ONLY; ";
+
+        private static string GetTransactionQuery = @" SELECT * FROM ApplicationTransactions WHERE Id = @Id ";
+
+
+        private static string DeleteTransactionQuery = @" DELETE FROM ApplicationTransactions WHERE Id = @Id ";
+
+
+        public void UpsertTransaction(ApplicationTransaction transaction)
+        {
+            using (var connection = _context.CreateConnection())
+            {               
+                connection.Execute(UpsertTransactionsQuery, transaction);
+            }
+        }
+
+        public IEnumerable<ApplicationTransaction> GetTransactions(int page, int pageSize)
+        {
+            using (var connection = _context.CreateConnection())
+            {
+
+                return connection.Query<ApplicationTransaction>(GetTransactionsQuery, new { PageSize = pageSize, Offset = (page - 1) * pageSize });
+            }
+        }
+
+        public ApplicationTransaction GetTransaction(Guid id)
+        {
+            using (var connection = _context.CreateConnection())
+            {
+               
+                return connection.QuerySingleOrDefault<ApplicationTransaction>(GetTransactionQuery, new { Id = id });
+            }
+        }
+
+        public void DeleteTransaction(Guid id)
+        {
+            using (var connection = _context.CreateConnection())
+            {               
+                connection.Execute(DeleteTransactionQuery, new { Id = id });
+            }
+        }
+
+
+    }
+}
