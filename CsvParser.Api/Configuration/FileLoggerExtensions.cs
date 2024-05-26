e11101afa09d66e87898ac9521cea088be478df3
Αναστάσιος Παλασόπουλος <a_palasopoulos@yahoo.com>
2024-05-27 02:08:29 +0300

    Configured Logging

---
+++ CsvParser.Api/Configuration/FileLoggerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using CsvParser.Common;
+
+namespace CsvParser.Api.Configuration
+{
+    public static class FileLoggerExtensions
+    {
+        public static ILoggingBuilder AddFile(this ILoggingBuilder builder, IConfiguration configuration)
+        {
+            builder.Services.Configure<FileLoggerOptions>(configuration);
+            builder.Services.AddSingleton<ILoggerProvider, FileLoggerProvider>();
+            return builder;
+        }
+    }
+}
---
+++ CsvParser.Common/FileLogger.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.Extensions.Logging;
+
+namespace CsvParser.Common
+{
+    public class FileLogger : ILogger
+    {
+        private readonly string _filePath;
+        private static readonly object _lock = new object();
+
+        public FileLogger(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public IDisposable BeginScope<TState>(TState state) => null;
+
+        public bool IsEnabled(LogLevel logLevel) => logLevel != LogLevel.None;
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            if (!IsEnabled(logLevel))
+                return;
+
+            var message = $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} [{logLevel}] {formatter(state, exception)}";
+            if (exception != null)
+                message += Environment.NewLine + exception;
+
+            lock (_lock)
+            {
+                File.AppendAllText(_filePath, message + Environment.NewLine);
+            }
+        }
+    }
+}
